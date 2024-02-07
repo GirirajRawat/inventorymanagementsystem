db88615324ae49028769202bebfbb60a9fe1859f
GirirajRawat <girirajrawat33@gmail.com>
2024-02-07 11:54:07 +0545

    changes on purchase and homescreen module

---
+++ screens/home_screen.py
@@ -0,0 +1,307 @@
+# from tkinter import *
+# from tkinter import font
+# root = Tk()  
+# root.title("Stock Panda")
+# root.config(bg="#8A908B")
+# root.geometry("1100x700")
+# root.maxsize(height=700, width=1100)    
+# root.minsize(height=700, width=1100) 
+
+
+# def whenPurchaseItem():
+#     import purchase_window
+   
+# def whenSaleItem():
+#     import sales_window 
+# def onEnter(e):
+#     search_bar.delete(0, "end")
+
+# def onLeave(e):
+#     search = search_bar.get()
+#     if search == "":
+#         search_bar.insert(0, "Search")
+
+
+# homeImg = PhotoImage(file="assets/home.png")
+# shoppingImg = PhotoImage(file="assets/shopping-cart.png")
+# dollarImg = PhotoImage(file="assets/dollar-sign.png")
+# userImg = PhotoImage(file="assets/user.png")
+# searchImg = PhotoImage(file="assets/search.png")
+# deleteImg = PhotoImage(file="assets/delete_icon.png")
+
+# # Custom text styles
+# customButtonFont = font.Font(size=16, weight="bold")
+# customTitleFont = font.Font(size=20, weight="bold")
+
+# # container in left side
+# container = Frame(bg="#D9D9D9", height=700, width=300)
+# container.pack_propagate(False)
+# container.pack(side=LEFT)
+
+# # to display image logo
+# imageLogo = PhotoImage(file="assets/imagelogo.png")
+# logoDispaly = Label(container, image=imageLogo, bg="#D9D9D9")
+# logoDispaly.place(x=65, y=30)
+
+# # to display app bar
+# appBar = Frame(root, height=60, width=800, bg="#7F7F7F")
+# appBar.pack_propagate(False)
+# appBar.pack()
+
+# # to display text logo
+# textlogo = PhotoImage(file="assets/textlogo.png")
+# logoDispaly = Label(appBar, image=textlogo, bg="#7F7F7F")
+# logoDispaly.place(x=30, y=10)
+
+# # search bar 
+# search_bar = Entry(appBar, width=22, font=7)
+# search_bar.insert(0, "Search")
+# search_bar.bind("<FocusIn>", onEnter)
+# search_bar.bind("<FocusOut>", onLeave)
+# search_bar.place(x=487, y=15, height=30)
+
+# search_btn = Button(appBar, image=searchImg, bg="#FF5252", )
+# search_btn.image = searchImg
+# search_btn.place(x=740, y=15)
+
+# # to add buttons to left
+# home_btn = Button(container, text="Home", image=homeImg, compound="left", height=35, width=260, bg="#FF5252",
+#                   fg="white", border=0, font=customButtonFont, )
+# home_btn.image = homeImg
+# home_btn.pack(pady=(280, 0))
+
+# purchase_btn = Button(container, text="Purchase", image=dollarImg, compound="left", height=35, width=260, bg="#FF5252",
+#                       fg="white", border=0, font=customButtonFont, command=lambda:whenPurchaseItem())
+# purchase_btn.image = dollarImg
+# purchase_btn.pack(pady=(10, 0))
+
+# saleBtn = Button(container, text="Sale", image=shoppingImg, compound="left", height=35, width=260, bg="#FF5252",
+#                  fg="white", border=0, font=customButtonFont, command=whenSaleItem )
+# saleBtn.image = shoppingImg
+# saleBtn.pack(pady=(10, 0))
+
+# account_btn = Button(container, text="Account", image=userImg, compound="left", height=35, width=260, bg="#FF5252",
+#                      fg="white", border=0, font=customButtonFont, )
+# account_btn.image = userImg
+# account_btn.pack(pady=(10, 0))
+
+# # for avialable stocks label
+# Label(root,text="Availabe Stocks",font=("Arial",18,"bold"),bg="#8A908B").place(x=320,y=95)
+
+# # for toal purchase, sales and stocks value
+# valueContainer= Frame(root)
+# valueContainer.place(x=874,y=100)
+
+# stocksValue = Label(valueContainer,text="Stocks:XXX")
+# stocksValue.grid(row=0,column=0)
+
+# purchaseValue = Label(valueContainer,text="Purchase:XXX")
+# purchaseValue.grid(row=0,column=1)
+
+# saleValue = Label(valueContainer,text="Sales:XXX")
+# saleValue.grid(row=0,column=2)
+
+# # frame to store list of avaialble stocks
+# stocks_frame=Frame(root,height=520,width=765,bg="#D9D9D9")
+# stocks_frame.pack_propagate(False)
+# stocks_frame.pack(padx=(6,0),pady=(70,0))
+
+# # frame to store labels
+# stocks_frame_labels=Frame(stocks_frame,height=520,width=760,bg="#D9D9D9")
+# stocks_frame_labels.place(x=50,y=10)
+
+# # labels inside stocks container
+# id_label = Label(stocks_frame_labels,text="ID",font=("Arial",13,"bold",),bg="#D9D9D9").grid(row=0,column=0,padx=(0,52))
+# productName_label = Label(stocks_frame_labels,text="Product Name",font=("Arial",13,"bold"),bg="#D9D9D9").grid(row=0,column=1,padx=(0,168))
+# qty_label=Label(stocks_frame_labels,text="Qty",font=("Arial",13,"bold"),bg="#D9D9D9").grid(row=0,column=2,padx=(0,70))
+# cp_label=Label(stocks_frame_labels,text="CP",font=("Arial",13,"bold"),bg="#D9D9D9").grid(row=0,column=3,padx=(0,76))
+# total_label=Label(stocks_frame_labels,text="Total",font=("Arial",13,"bold"),bg="#D9D9D9").grid(row=0,column=4,padx=(0,0))
+
+
+# # for scroll frame
+# scroll_frame=Frame(stocks_frame,bg="pink",height=480,width=765)
+# scroll_frame.pack_propagate(False)
+# scroll_frame.pack(side=BOTTOM)
+
+# # stock items list tile frame
+# itemFrame= Frame(scroll_frame,bg="red",height=34,width=750)
+# itemFrame.pack_propagate(False)
+# itemFrame.pack(pady=10)
+
+# delete_btn = Button(itemFrame, image=deleteImg )
+# delete_btn.image = deleteImg
+# delete_btn.place(x=1,y=2,)
+
+# item_id_field = Entry(itemFrame,width=6,font=10)
+# item_id_field.place(x=39,y=2,height=30)
+
+# item_name_field = Entry(itemFrame,width=25,font=10)
+# item_name_field.place(x=116,y=2,height=30)
+
+# item_qty_field = Entry(itemFrame,width=9,font=10)
+# item_qty_field.place(x=401,y=2,height=30)
+
+# item_cp_field = Entry(itemFrame,width=9,font=10)
+# item_cp_field.place(x=510,y=2,height=30)
+
+# item_total_field = Entry(itemFrame,width=11,font=10)
+# item_total_field.place(x=620,y=2,height=30)
+
+
+
+
+# root.mainloop()
+
+
+from tkinter import *
+from tkinter import font
+import purchase_window
+
+root = Tk()
+root.title("Stock Panda")
+root.config(bg="#8A908B")
+root.geometry("1100x700")
+root.maxsize(height=700, width=1100)
+root.minsize(height=700, width=1100)
+
+def whenPurchaseItem():
+    purchase_window.create_purchase_window(root)
+
+def whenSaleItem():
+    import sales_window
+def onEnter(e):
+    search_bar.delete(0, "end")
+
+def onLeave(e):
+    search = search_bar.get()
+    if search == "":
+        search_bar.insert(0, "Search")
+
+
+homeImg = PhotoImage(file="assets/home.png")
+shoppingImg = PhotoImage(file="assets/shopping-cart.png")
+dollarImg = PhotoImage(file="assets/dollar-sign.png")
+userImg = PhotoImage(file="assets/user.png")
+searchImg = PhotoImage(file="assets/search.png")
+deleteImg = PhotoImage(file="assets/delete_icon.png")
+
+# Custom text styles
+customButtonFont = font.Font(size=16, weight="bold")
+customTitleFont = font.Font(size=20, weight="bold")
+
+# container in left side
+container = Frame(bg="#D9D9D9", height=700, width=300)
+container.pack_propagate(False)
+container.pack(side=LEFT)
+
+# to display image logo
+imageLogo = PhotoImage(file="assets/imagelogo.png")
+logoDispaly = Label(container, image=imageLogo, bg="#D9D9D9")
+logoDispaly.place(x=65, y=30)
+
+# to display app bar
+appBar = Frame(root, height=60, width=800, bg="#7F7F7F")
+appBar.pack_propagate(False)
+appBar.pack()
+
+# to display text logo
+textlogo = PhotoImage(file="assets/textlogo.png")
+logoDispaly = Label(appBar, image=textlogo, bg="#7F7F7F")
+logoDispaly.place(x=30, y=10)
+
+# search bar 
+search_bar = Entry(appBar, width=22, font=7)
+search_bar.insert(0, "Search")
+search_bar.bind("<FocusIn>", onEnter)
+search_bar.bind("<FocusOut>", onLeave)
+search_bar.place(x=487, y=15, height=30)
+
+search_btn = Button(appBar, image=searchImg, bg="#FF5252", )
+search_btn.image = searchImg
+search_btn.place(x=740, y=15)
+
+# to add buttons to left
+home_btn = Button(container, text="Home", image=homeImg, compound="left", height=35, width=260, bg="#FF5252",
+                  fg="white", border=0, font=customButtonFont, )
+home_btn.image = homeImg
+home_btn.pack(pady=(280, 0))
+
+purchase_btn = Button(container, text="Purchase", image=dollarImg, compound="left", height=35, width=260, bg="#FF5252",
+                      fg="white", border=0, font=customButtonFont, command=whenPurchaseItem)
+purchase_btn.image = dollarImg
+purchase_btn.pack(pady=(10, 0))
+
+saleBtn = Button(container, text="Sale", image=shoppingImg, compound="left", height=35, width=260, bg="#FF5252",
+                 fg="white", border=0, font=customButtonFont, command=whenSaleItem )
+saleBtn.image = shoppingImg
+saleBtn.pack(pady=(10, 0))
+
+account_btn = Button(container, text="Account", image=userImg, compound="left", height=35, width=260, bg="#FF5252",
+                     fg="white", border=0, font=customButtonFont, )
+account_btn.image = userImg
+account_btn.pack(pady=(10, 0))
+
+# for avialable stocks label
+Label(root,text="Availabe Stocks",font=("Arial",18,"bold"),bg="#8A908B").place(x=320,y=95)
+
+# for toal purchase, sales and stocks value
+valueContainer= Frame(root)
+valueContainer.place(x=874,y=100)
+
+stocksValue = Label(valueContainer,text="Stocks:XXX")
+stocksValue.grid(row=0,column=0)
+
+purchaseValue = Label(valueContainer,text="Purchase:XXX")
+purchaseValue.grid(row=0,column=1)
+
+saleValue = Label(valueContainer,text="Sales:XXX")
+saleValue.grid(row=0,column=2)
+
+# frame to store list of avaialble stocks
+stocks_frame=Frame(root,height=520,width=765,bg="#D9D9D9")
+stocks_frame.pack_propagate(False)
+stocks_frame.pack(padx=(6,0),pady=(70,0))
+
+# frame to store labels
+stocks_frame_labels=Frame(stocks_frame,height=520,width=760,bg="#D9D9D9")
+stocks_frame_labels.place(x=50,y=10)
+
+# labels inside stocks container
+id_label = Label(stocks_frame_labels,text="ID",font=("Arial",13,"bold",),bg="#D9D9D9").grid(row=0,column=0,padx=(0,52))
+productName_label = Label(stocks_frame_labels,text="Product Name",font=("Arial",13,"bold"),bg="#D9D9D9").grid(row=0,column=1,padx=(0,168))
+qty_label=Label(stocks_frame_labels,text="Qty",font=("Arial",13,"bold"),bg="#D9D9D9").grid(row=0,column=2,padx=(0,70))
+cp_label=Label(stocks_frame_labels,text="CP",font=("Arial",13,"bold"),bg="#D9D9D9").grid(row=0,column=3,padx=(0,76))
+total_label=Label(stocks_frame_labels,text="Total",font=("Arial",13,"bold"),bg="#D9D9D9").grid(row=0,column=4,padx=(0,0))
+
+
+# for scroll frame
+scroll_frame=Frame(stocks_frame,bg="pink",height=480,width=765)
+scroll_frame.pack_propagate(False)
+scroll_frame.pack(side=BOTTOM)
+
+# stock items list tile frame
+itemFrame= Frame(scroll_frame,bg="red",height=34,width=750)
+itemFrame.pack_propagate(False)
+itemFrame.pack(pady=10)
+
+delete_btn = Button(itemFrame, image=deleteImg )
+delete_btn.image = deleteImg
+delete_btn.place(x=1,y=2,)
+
+item_id_field = Entry(itemFrame,width=6,font=10)
+item_id_field.place(x=39,y=2,height=30)
+
+item_name_field = Entry(itemFrame,width=25,font=10)
+item_name_field.place(x=116,y=2,height=30)
+
+item_qty_field = Entry(itemFrame,width=9,font=10)
+item_qty_field.place(x=401,y=2,height=30)
+
+item_cp_field = Entry(itemFrame,width=9,font=10)
+item_cp_field.place(x=510,y=2,height=30)
+
+item_total_field = Entry(itemFrame,width=11,font=10)
+item_total_field.place(x=620,y=2,height=30)
+
+
+root.mainloop()
---
+++ screens/signup_screen.py
@@ -0,0 +1,153 @@
+from tkinter import *
+from tkinter import font
+
+root = Tk()
+root.title("Stock Panda")
+root.config(bg="#8A908B")
+root.geometry("1100x700")
+root.maxsize(height=700, width=1100)
+root.minsize(height=700, width=1100)
+
+
+
+# # creating a icon path
+icon_path = "assets/stockpanda1.ico"
+
+# # using iconpath
+root.iconbitmap(icon_path)
+
+
+# icon_image path
+callImg = PhotoImage(file='assets/telephone.png')
+eyeImg = PhotoImage(file='assets/eye_view_icon.png')
+eyeSlashImg = PhotoImage(file='assets/eye_slash_icon.png')
+
+
+def password_visibility():
+    if password.cget('show') == '*':
+        password.config(show="")
+        eye_label.config(image=eyeImg)
+    else:
+        password.config(show="*")
+        eye_label.config(image=eyeSlashImg)
+
+
+def confirm_password_visibility():
+    if confirmPassword.cget('show') == '*':
+        confirmPassword.config(show="")
+        eye_label_2.config(image=eyeImg)
+    else:
+        confirmPassword.config(show="*")
+        eye_label_2.config(image=eyeSlashImg)
+
+
+def alreadyHaveAccount():
+    root.destroy()
+    import login_screen
+
+
+def PhoneOnEnter(e):
+    phone.delete(0, "end")
+
+
+def PhoneOnLeave(e):
+    phoneData = phone.get()
+    if phoneData == "":
+        phone.insert(0, "  Phone Number")
+
+
+def PassOnEnter(e):
+    password.delete(0, "end")
+
+
+def PassOnLeave(e):
+    passwordData = password.get()
+    if passwordData == "":
+        password.insert(0, "  Password")
+
+
+def ConfPassOnEnter(e):
+    confirmPassword.delete(0, "end")
+
+
+def ConfPassOnLeave(e):
+    confPasswordData = confirmPassword.get()
+    if confPasswordData == "":
+        confirmPassword.insert(0, "  Confirm Password")
+
+
+# Custom text styles
+customButtonFont = font.Font(size=16, weight="bold")
+customTitleFont = font.Font(size=20, weight="bold")
+
+# for hero text
+Label(root, text="Stock Panda", bg="#8A908B", font=(
+    "Arial", 30, "bold"), fg="white").place(x=30, y=290)
+Label(root, text="Some Description about the software", bg="#8A908B",
+      font=("Arial", 20), fg="white").place(x=30, y=340)
+
+# to display text logo
+textlogo = PhotoImage(file="assets/textlogo.png")
+logoDispaly = Label(root, image=textlogo, bg="#8A908B")
+logoDispaly.place(x=30, y=20)
+
+container = Frame(bg="#D9D9D9", height=700, width=450)
+container.pack_propagate(False)
+container.pack(side=RIGHT)
+
+# to display image logo
+imageLogo = PhotoImage(file="assets/imagelogo.png")
+logoDispaly = Label(container, image=imageLogo, bg="#D9D9D9")
+logoDispaly.place(x=150, y=50)
+
+# Welcome text
+h1 = Label(container, text="Create an Account",
+           bg="#D9D9D9", font=customTitleFont)
+h1.place(x=120, y=230)
+
+# for text fields
+phone = Entry(container, width=27, font=(9))
+phone.insert(0, "  Phone Number")
+phone.bind("<FocusIn>", PhoneOnEnter)
+phone.bind("<FocusOut>", PhoneOnLeave)
+phone.place(x=90, y=300, height=38)
+
+password = Entry(container, width=27, font=(9))
+password.insert(0, "  Password")
+password.bind("<FocusIn>", PassOnEnter)
+password.bind("<FocusOut>", PassOnLeave)
+password.place(x=90, y=350, height=38)
+
+confirmPassword = Entry(container, width=27, font=(9))
+confirmPassword.insert(0, "  Confirm Password")
+confirmPassword.bind("<FocusIn>", ConfPassOnEnter)
+confirmPassword.bind("<FocusOut>", ConfPassOnLeave)
+confirmPassword.place(x=90, y=400, height=38)
+
+# icon label_call
+icon_label_call = Label(root, image=callImg, cursor='hand2',bg="white")
+icon_label_call.place(in_=phone, relx=1.0, rely=0.0, anchor='ne')
+
+# icon label eye and eyeslash for password
+eye_label = Label(root, image=eyeImg, cursor='hand2',bg="white")
+eye_label.place(in_=password, relx=1.0, rely=0.0, anchor='ne')
+eye_label.bind("<Button-1>", lambda event: password_visibility())
+
+
+# # icon label eye and eyeslash for confirm password
+eye_label_2 = Label(root, image=eyeImg, cursor='hand2',bg="white")
+eye_label_2.place(in_=confirmPassword, relx=1.0, rely=0.0, anchor='ne')
+eye_label_2.bind("<Button-1>", lambda event: confirm_password_visibility())
+
+
+# for login button
+login = Button(container, text="Log in", height=1, width=22,
+               border=0, bg="#FF5252", fg="white", font=customButtonFont)
+login.place(x=96, y=520)
+
+# already have an account button
+signUp = Button(container, text="Already have an account", border=0, fg="blue", bg="#D9D9D9", font=1,
+                command=alreadyHaveAccount)
+signUp.place(x=130, y=650)
+
+root.mainloop()
